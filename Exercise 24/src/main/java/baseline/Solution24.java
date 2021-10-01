e8523f99f91a06b28f9ee9e84eaacd47c9ba9fd3
Fareed <fareedally1999@gmail.com>
2021-10-01 14:41:33 -0400

    added exercise 24 psuedocode

---
+++ Exercise 24/src/main/java/baseline/Solution24.java
@@ -0,0 +1,44 @@
+// Ask the user to enter two strings (IN METHOD)
+// Convert string to array (IN METHOD)
+// Create anagram (IN METHOD)
+
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution24
+{
+    private static final Scanner in = new Scanner(System.in);
+
+    private String input (String prompt)
+    {
+
+    }
+
+    private char[] convert(String userInput)
+    {
+
+    }
+
+
+    private boolean isAnagram (String word1, String word2)
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+     // Print first statement
+
+     // Call input function to get two strings in a loop
+
+     // Call isAnagram to create anagram
+
+     // ???
+
+     // Profit    
+    }
+
+
+}
