9a7abdc8e4402b59b506c4516359dd2d8e0ef747
Fareed <fareedally1999@gmail.com>
2021-10-02 01:36:49 -0400

    completed exercise 25

--- Exercise 25/src/main/java/baseline/Solution25.java
+++ Exercise 25/src/main/java/baseline/Solution25.java
@@ -1,3 +1,8 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Fareed Ally
+ */
+
 // Ask user for password (IN METHOD)
 // Determine the strength of the password based on input (IN METHOD)
 // Return the strength of password (IN METHOD)
@@ -10,21 +15,85 @@ public class Solution25
 {
     private static final Scanner in = new Scanner(System.in);
 
+    private String password;
+    private int strength;
+    private String passStrength;
+
     private String input(String prompt)
     {
-
+        return in.nextLine();
     }
 
-    private int evaluatePassword(String password)
+    public int passwordValidator(String password)
     {
-        // Create boolean variables that tell what the password contains
-        // Use if statements to set variables to true if they in password
-        // Use if statements to assign strength level based on complexity
+        boolean letters = false;
+        boolean numbers = false;
+        boolean special = false;
+
+        char[] temp = password.toCharArray();
+
+        for(char i : temp)
+        {
+            if(Character.isAlphabetic(i))
+            {
+                letters = true;
+            }
+            else if(Character.isDigit(i))
+            {
+                numbers = true;
+            }
+            else if((!Character.isDigit(i)) && (!Character.isAlphabetic(i)))
+            {
+                special = true;
+            }
+        }
+
+        if(numbers && !letters && temp.length<8)
+        {
+            strength = 1;
+        }
+        else if(letters && !numbers && temp.length<8)
+        {
+            strength = 2;
+        }
+        else if(letters && numbers && !special && temp.length>=8)
+        {
+            strength = 3;
+        }
+        else if(letters && numbers && special && temp.length>=8)
+        {
+            strength = 4;
+        }
+        else
+        {
+            strength = 0;
+        }
+
+        return strength;
     }
 
     private String passwordStrength(int strength)
     {
-        // Use if statements based on strength to return string that says how strong password is
+        String temp = "";
+
+        if(strength == 1)
+        {
+            temp = "Very Weak";
+        }
+        if(strength == 2)
+        {
+            temp = "Weak";
+        }
+        if(strength == 3)
+        {
+            temp = "Strong";
+        }
+        if(strength == 4)
+        {
+            temp = "Very Strong";
+        }
+
+        return temp;
     }
 
 
@@ -33,6 +102,12 @@ public class Solution25
     {
         Solution25 main = new Solution25();
 
-        // Call functions, assign a value to evalpassword function, and print string from passwordStrength
+        main.password = main.input("Enter password: ");
+
+        main.strength = main.passwordValidator(main.password);
+
+        main.passStrength = main.passwordStrength(main.strength);
+
+        System.out.printf("The password %s is a %s password. ", main.password, main.passStrength);
     }
 }
