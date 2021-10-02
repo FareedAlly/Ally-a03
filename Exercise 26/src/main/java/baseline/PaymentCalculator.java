0054a1f19b128d7c8579a8343772053757fb67df
Fareed <fareedally1999@gmail.com>
2021-10-02 11:51:00 -0400

    added exercise 26 pseudocode

---
+++ Exercise 26/src/main/java/baseline/PaymentCalculator.java
@@ -0,0 +1,11 @@
+package baseline;
+
+public class PaymentCalculator
+{
+    private int calculateMonthsUntilPaidOff()
+    {
+        // Convert apr to a decimal value
+        // Use formula to calculate months
+        // Return months
+    }
+}
---
+++ Exercise 26/src/main/java/baseline/Solution26.java
@@ -0,0 +1,24 @@
+// Ask user for balance and card and monthly payment (IN METHOD)
+// Call calculateMonthsUntilPaidOff from other class
+// Print final result
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution26
+{
+    private static final Scanner in = new Scanner(System.in);
+
+    private int input(String prompt)
+    {
+
+    }
+
+    public static void main(String[] args)
+    {
+        Solution26 main = new Solution26();
+
+    }
+
+}
