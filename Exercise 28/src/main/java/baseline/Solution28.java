48a7028bee05136bf093cde0573d7fc5ed9c1fcd
Fareed <fareedally1999@gmail.com>
2021-10-02 18:23:44 -0400

    added exercise 28 pseudocode

---
+++ Exercise 28/src/main/java/baseline/Solution28.java
@@ -0,0 +1,28 @@
+// Call input to get the 5 numbers from user (IN METHOD)
+// Call sum to sum all the numbers in the array (IN METHOD)
+// Print result
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution28
+{
+    private static final Scanner in = new Scanner(System.in);
+
+    private int input(int[] numbers)
+    {
+        // Use for loop to iterate 5 times and store values in an array
+    }
+
+    private int sum(int[] numbers)
+    {
+        // Use for loop to add each item in the array
+    }
+
+    public static void main(String[] args)
+    {
+
+
+    }
+}
