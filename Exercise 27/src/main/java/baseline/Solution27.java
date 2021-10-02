368975240f4735bb8ec1f3e5f8cdc08a68f1faab
Fareed <fareedally1999@gmail.com>
2021-10-02 13:24:16 -0400

    added exercise 27 pseudocode

---
+++ Exercise 27/src/main/java/baseline/Solution27.java
@@ -0,0 +1,57 @@
+// Ask user for all the inputs (IN METHOD)
+// Call validateInput to check inputs (IN METHOD)
+// Print result
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution27
+{
+    private static final Scanner in = new Scanner(System.in);
+
+    private String validateInput(String first, String last, String zip, String ID)
+    {
+        // Use a bunch of boolean values for each input
+        // Use functions to set these values to true or false based on if they are present
+        // Use if statements to print out the results based on what is present
+    }
+
+    private boolean firstNamePresent(String first)
+    {
+        // Check to see if user entered first name
+    }
+
+    private boolean lastNamePresent(String last)
+    {
+        // Check to see if user entered last name
+    }
+
+    private boolean checkFirstName(String first)
+    {
+        // Check to see if first name fits criteria
+    }
+
+    private boolean checkLastName(String last)
+    {
+
+    }
+
+    private boolean checkZip(String zip)
+    {
+        // Check to see if all is numbers
+    }
+
+    private boolean checkID(String ID)
+    {
+        // Use if statement to check the length as well as the necessary characters in each place in the array
+    }
+
+    public static void main(String[] args)
+    {
+        // Get inputs
+        // Call validateInput to get result
+        // Print result
+
+    }
+}
