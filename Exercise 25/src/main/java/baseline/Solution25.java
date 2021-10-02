bc0d586f5faea8e2b0b6ec926e8ee5454250b5e8
Fareed <fareedally1999@gmail.com>
2021-10-01 21:57:33 -0400

    added exercise 25 pseudocode

---
+++ Exercise 25/src/main/java/baseline/Solution25.java
@@ -0,0 +1,38 @@
+// Ask user for password (IN METHOD)
+// Determine the strength of the password based on input (IN METHOD)
+// Return the strength of password (IN METHOD)
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution25
+{
+    private static final Scanner in = new Scanner(System.in);
+
+    private String input(String prompt)
+    {
+
+    }
+
+    private int evaluatePassword(String password)
+    {
+        // Create boolean variables that tell what the password contains
+        // Use if statements to set variables to true if they in password
+        // Use if statements to assign strength level based on complexity
+    }
+
+    private String passwordStrength(int strength)
+    {
+        // Use if statements based on strength to return string that says how strong password is
+    }
+
+
+
+    public static void main(String[] args)
+    {
+        Solution25 main = new Solution25();
+
+        // Call functions, assign a value to evalpassword function, and print string from passwordStrength
+    }
+}
