15529b328253d6e5288da6000e7a25ef22bc7f61
Fareed <fareedally1999@gmail.com>
2021-10-03 03:35:32 -0400

    added exercise 31 pseudocode

---
+++ Exercise 31/src/main/java/baseline/Solution31.java
@@ -0,0 +1,27 @@
+// Get age and heart rate (IN METHOD)
+// Use string builder to print results in table (IN METHOD)
+
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution31
+{
+    private static final Scanner in = new Scanner(System.in);
+
+    private int input(String prompt)
+    {
+        // Use try and catch to get valid inputs
+    }
+
+    private StringBuilder output(int age, int heartbeat)
+    {
+        // Append statements and use for loop to get heart rate at different intensities
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
+}
