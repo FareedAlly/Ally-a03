59b9a3c394ae12553ab01cc36415c351481d0e84
Fareed <fareedally1999@gmail.com>
2021-10-03 03:05:26 -0400

    completed exercise 30

--- Exercise 30/src/main/java/baseline/Solution30.java
+++ Exercise 30/src/main/java/baseline/Solution30.java
@@ -1,3 +1,11 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Fareed Ally
+ */
+
+// Store values in 2d array (IN METHOD)
+// Print out results
+
 package baseline;
 
 import java.util.Scanner;
@@ -6,16 +14,38 @@ public class Solution30
 {
     private static final Scanner in = new Scanner(System.in);
 
+    private int[][] table;
+
     private int[][] createTable(int row, int column)
     {
-        // Use nested for loops to create multiplication table
+        int[][] temp = new int[row][column];
+
+        for(int i=0; i<temp.length; i++)
+        {
+            for(int j=0; j<temp[i].length; j++)
+            {
+                temp[i][j] = (i+1) * (j+1);
+            }
+        }
+
+        return temp;
     }
 
     public static void main(String[] args)
     {
-        // Create 2d array
-        // Call createTable to populate values
-        // Print results
+        Solution30 main = new Solution30();
+
+        main.table = main.createTable(12,12);
+
+        for (int[] numbers : main.table)
+        {
+            for (int singleNumber : numbers)
+            {
+                System.out.printf(" %3d ", singleNumber);
+            }
+
+            System.out.println();
+        }
     }
 
 }
