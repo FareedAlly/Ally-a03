d8f5098f18b56935ce6922f2ac6e86a4df5a7088
Fareed <fareedally1999@gmail.com>
2021-10-02 20:30:44 -0400

    added exercise 29 pseudocode

---
+++ Exercise 29/src/main/java/baseline/Solution29.java
@@ -0,0 +1,23 @@
+// Calculate rate of return (IN METHOD)
+// Print output
+
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution29
+{
+    private static final Scanner in = new Scanner(System.in);
+
+    private void output()
+    {
+        // Use boolean value to track if user entered valid input
+        // Use if statements to check if the input is a 0 or alphabetic
+        // If everything is fine then calculate result
+    }
+
+    public static void main(String[] args)
+    {
+
+    }
+}
