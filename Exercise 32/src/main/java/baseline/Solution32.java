a4cca44374b8146969b8ce59c6e1c7a55126f3f0
Fareed <fareedally1999@gmail.com>
2021-10-03 12:19:10 -0400

    added exercise 32 pseudocode

---
+++ Exercise 32/src/main/java/baseline/Solution32.java
@@ -0,0 +1,36 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution32
+{
+    private static final Scanner in = new Scanner(System.in);
+
+    private int difficultyLevel()
+    {
+        // Ask user for difficulty level
+        // Use if statement to force user to enter a valid number
+        // Return difficulty level
+    }
+
+    private int random(int level)
+    {
+        // Based on level, use if statements to return a random number
+    }
+
+    private boolean guessingGame(int rng)
+    {
+        // Use a boolean value as a tracker, mark when guessed correctly
+        // Use if statements to make user enter valid input
+        // Use if statements to track what the guess is in relation to the random number
+    }
+
+    public static void main(String[] args)
+    {
+
+        // Use boolean value to track whether or not game is done
+        // Use while loop to make sure it stops when number is guessed
+        // Play game and call functions
+
+    }
+}
