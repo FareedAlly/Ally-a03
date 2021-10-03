4322140a76824472592946eec4ab10897f954efd
Fareed <fareedally1999@gmail.com>
2021-10-03 16:00:21 -0400

    added exercise 35 pseudocode

---
+++ Exercise 35/src/main/java/baseline/Solution35.java
@@ -0,0 +1,40 @@
+package baseline;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class Solution35
+{
+    private static final Scanner in = new Scanner(System.in);
+
+    private String getName()
+    {
+        // Ask for a name
+        // Return name
+    }
+
+    private String randomIndex(int length)
+    {
+        // generate random index from array with all names and return it
+    }
+
+    private String winner(ArrayList<String> names, int random)
+    {
+        // Return a random name based on the random index from the list of names
+    }
+
+    public static void main(String[] args)
+    {
+        Solution35 main = new Solution35();
+
+        // Create an ArrayList
+        // Use while loop to keep getting names
+        // Break loop if space entered
+        // Call function to generate random index
+        // Call function to select name based on random index
+        // Print result
+
+    }
+
+}
