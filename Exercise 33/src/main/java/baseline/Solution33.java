d1970d72a1020e5a94bc38b24ed31a4b1faeafed
Fareed <fareedally1999@gmail.com>
2021-10-03 13:24:08 -0400

    added exercise 33 pseudocode

---
+++ Exercise 33/src/main/java/baseline/Solution33.java
@@ -0,0 +1,37 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution33
+{
+    private static final Scanner in = new Scanner(System.in);
+
+    private String answer()
+    {
+        // Store answers in array
+        // Use rng and use if statements to match each random number with a response from the array
+        // Return response
+    }
+
+    private void initialQuestion()
+    {
+        // Ask for question
+        // Store question
+    }
+
+    private boolean subsequentQuestions()
+    {
+        // Ask if they want another question
+        // Use boolean value to track and see if the user enters yes
+        // Store new question
+    }
+
+    public static void main(String[] args)
+    {
+
+        // Use boolean value to track if game is done like last problem
+        // Use while loop to call all functions while game is still going on like last problem
+
+    }
+
+}
