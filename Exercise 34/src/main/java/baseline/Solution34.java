392f6a465e57adc2b24cbe8dd3476d35deb02595
Fareed <fareedally1999@gmail.com>
2021-10-03 15:00:45 -0400

    completed exercise 34

--- Exercise 34/src/main/java/baseline/Solution34.java
+++ Exercise 34/src/main/java/baseline/Solution34.java
@@ -1,26 +1,42 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Fareed Ally
+ */
+
 package baseline;
 
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Solution34
 {
     private static final Scanner in = new Scanner(System.in);
 
-    private String input()
+    private String removeEmployee()
     {
-        // Ask user which employee name to remove
-        // Return that string
+        System.out.printf("%nEnter an employee name to remove: ");
+
+        return in.nextLine();
     }
 
     public static void main(String[] args)
     {
         Solution34 main = new Solution34();
 
-        // Create an arraylist that holds all employees
-        // Print out list
-        // Ask user which name to remove using function
-        // Remove that employee
-        // Print final list
+        ArrayList<String> employees = new ArrayList<>();
 
+        employees.add("John Smith");
+        employees.add("Jackie Jackson");
+        employees.add("Chris Jones");
+        employees.add("Amanda Cullen");
+        employees.add("Jeremy Goodwin");
+
+        System.out.printf("There are %d employees:%n%s%n%s%n%s%n%s%n%s%n", employees.size(), employees.get(0), employees.get(1), employees.get(2), employees.get(3), employees.get(4));
+
+        String employeeToRemove = main.removeEmployee();
+
+        employees.remove(employeeToRemove);
+
+        System.out.printf("%nThere are %d employees:%n%s%n%s%n%s%n%s", employees.size(), employees.get(0), employees.get(1), employees.get(2), employees.get(3));
     }
 }
