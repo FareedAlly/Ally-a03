1dc5b5f8b53489f387023cf0cf95d2b00b851ce4
Fareed <fareedally1999@gmail.com>
2021-10-03 14:53:30 -0400

    added exercise 34 pseudocode

---
+++ Exercise 34/src/main/java/baseline/Solution34.java
@@ -0,0 +1,26 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution34
+{
+    private static final Scanner in = new Scanner(System.in);
+
+    private String input()
+    {
+        // Ask user which employee name to remove
+        // Return that string
+    }
+
+    public static void main(String[] args)
+    {
+        Solution34 main = new Solution34();
+
+        // Create an arraylist that holds all employees
+        // Print out list
+        // Ask user which name to remove using function
+        // Remove that employee
+        // Print final list
+
+    }
+}
