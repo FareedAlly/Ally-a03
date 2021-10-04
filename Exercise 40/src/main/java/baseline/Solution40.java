5f8b2b8f80de3c2a4d1649d506eb29d6cb2f32fb
Fareed <fareedally1999@gmail.com>
2021-10-03 23:26:54 -0400

    added exercise 40 pseudocode

---
+++ Exercise 40/src/main/java/baseline/Solution40.java
@@ -0,0 +1,64 @@
+package baseline;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.PrimitiveIterator;
+import java.util.Scanner;
+
+public class Solution40
+{
+    private static final Scanner in = new Scanner(System.in);
+
+    public HashMap firstNameMap(String[] lastNames)
+    {
+        // Create hashmap for the firstnames
+        // Put them in order based on lastname
+        // Return hashmap
+    }
+
+    public HashMap positionMap(String[] lastNames)
+    {
+        // Create hashmap for the position
+        // Put them in order based on lastname
+        // Return hashmap
+    }
+
+    public HashMap seperationDateMap(String[] lastNames)
+    {
+        // Create hashmap for the seperation date
+        // Put them in order based on lastname
+        // Return hashmap
+    }
+
+    private String input(String prompt)
+    {
+        // Ask user to enter search string
+        // Return input
+    }
+
+    public ArrayList<String> searchTable(ArrayList<HashMap<String, String>> employees, String[] lastNames, String searchString)
+    {
+        // Use for loop to compare each last name with the input from user
+        // Return searhed strings
+    }
+
+    private void printTable(ArrayList<HashMap<String, String>> employees, String[] lastName, ArrayList<String> searchedLastName)
+    {
+        // Assign hashmaps to each section
+        // Print format
+        // Use a for loop to print all the searched data
+    }
+
+    public static void main(String[] args)
+    {
+        Solution40 main = new Solution40();
+
+        // Create String with all last names
+        // Create hashmap and create values from function
+        // Let user search string input from function
+        // Search the table using function
+        // Print table using function
+
+
+    }
+}
