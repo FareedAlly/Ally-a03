690a45477e9ff4ddd985423cc5b62325efb4ef33
Fareed <fareedally1999@gmail.com>
2021-10-03 23:11:36 -0400

    completed exercise 39

--- Exercise 39/src/main/java/baseline/Solution39.java
+++ Exercise 39/src/main/java/baseline/Solution39.java
@@ -1,49 +1,82 @@
 package baseline;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Scanner;
 
 public class Solution39
 {
-    private static final Scanner in = new Scanner(System.in);
-
-    private HashMap firstNameMap(String[] lastNames)
+    public HashMap firstNameMap(String[] lastNames)
     {
-        // Create hashmap for the firstnames
-        // Put them in order based on lastname
-        // Return hashmap
+        HashMap<String, String> firstNames = new HashMap<>();
+
+        firstNames.put(lastNames[0],"John");
+        firstNames.put(lastNames[1],"Tou");
+        firstNames.put(lastNames[2],"Michaela");
+        firstNames.put(lastNames[3], "Jake");
+        firstNames.put(lastNames[4], "Jacquelyn");
+        firstNames.put(lastNames[5], "Sally");
+
+        return firstNames;
     }
 
-    private HashMap positionMap(String[] lastNames)
+    public HashMap positionMap(String[] lastNames)
     {
-        // Create hashmap for the position
-        // Put them in order based on lastname
-        // Return hashmap
+        HashMap<String, String> positions = new HashMap<>();
+
+        positions.put(lastNames[0], "Manager");
+        positions.put(lastNames[1], "Software Engineer");
+        positions.put(lastNames[2], "District Manager");
+        positions.put(lastNames[3], "Programmer");
+        positions.put(lastNames[4], "DBA");
+        positions.put(lastNames[5], "Web Developer");
+
+        return positions;
     }
 
-    private HashMap seperationDateMap(String[] lastNames)
+    public HashMap seperationDateMap(String[] lastNames)
     {
-        // Create hashmap for the seperation date
-        // Put them in order based on lastname
-        // Return hashmap
+        HashMap<String, String> separationDate = new HashMap<>();
+
+        separationDate.put(lastNames[0], "2016-12-31");
+        separationDate.put(lastNames[1], "2016-10-05");
+        separationDate.put(lastNames[2], "2015-12-19");
+        separationDate.put(lastNames[3], " ");
+        separationDate.put(lastNames[4], " ");
+        separationDate.put(lastNames[5], "2015-12-18");
+
+        return separationDate;
     }
 
     private void printTable(ArrayList<HashMap<String, String>> employees, String[] lastNames)
     {
-        // Put hashmaps in order based on table
-        // Print format for table
-        // Use for loop to print each value stored in each hashmap at a specific index
+        HashMap<String, String> firstNames = employees.get(0);
+        HashMap<String, String> positions = employees.get(1);
+        HashMap<String, String> separationDate = employees.get(2);
+
+        System.out.format("%-20s| %-18s| %-15s%n", "Name", "Position", "Separation Date");
+        System.out.println("--------------------------------------------------------");
+
+        for (String index : lastNames)
+        {
+            System.out.format("%-20s| %-18s| %-15s%n", firstNames.get(index) + " " + index, positions.get(index), separationDate.get(index));
+        }
     }
 
     public static void main(String[] args)
     {
         Solution39 main = new Solution39();
 
-        // Create string that has the last names
-        // Create hashmap and use function to add data for each section
-        // Sort by alphabetic order
-        // Call function to print out table
+        String[] lastNames =  {"Johnson", "Xiong", "Michaelson", "Jacobson", "Jackson", "Webber"};
 
+        ArrayList<HashMap<String, String>> complete = new ArrayList<>();
+
+        complete.add(main.firstNameMap(lastNames));
+        complete.add(main.positionMap(lastNames));
+        complete.add(main.seperationDateMap(lastNames));
+
+        Arrays.sort(lastNames);
+
+        main.printTable(complete, lastNames);
     }
 }
