a4c04a0c40769ee641d71f2f1414b03620ae2ee2
Fareed <fareedally1999@gmail.com>
2021-10-03 20:36:48 -0400

    added exercise 36 pseudocode

---
+++ Exercise 36/src/main/java/baseline/Solution36.java
@@ -0,0 +1,59 @@
+package baseline;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class Solution36
+{
+    private static final Scanner in = new Scanner(System.in);
+
+    private double input()
+    {
+        // Ask user to enter number and store value
+        // Use if statement to see if user enter done and stop asking for values
+        // Use if statements to make sure user enters valid inputs
+    }
+
+    private double average(ArrayList<Double> numbers)
+    {
+        // Add up all the values using a for loop
+        // Divide by amount of numbers and return value
+    }
+
+    private double max(ArrayList<Double> numbers)
+    {
+        // Use for loop and if statements to check if current value is higher than max value
+        // If it is, then assign it as max value and return it
+    }
+
+    private double min(ArrayList<Double> numbers)
+    {
+        // Same premise as max function except checking for min
+    }
+
+    private double std(ArrayList<Double> numbers, double numAverage)
+    {
+        // Calculate the standard deviation of each number in array
+        // return value
+    }
+
+    private void printAllNumbers(ArrayList<Double> numbers)
+    {
+        // Use for loop to print each number in array
+    }
+
+
+
+    public static void main(String[] args)
+    {
+        Solution36 main = new Solution36();
+
+        // Create arrayList
+        // Use while loop to keep getting numbers by calling function
+        // Break when done is entered
+        // Calculate all the values by calling the respective functions
+        // Print all the numbers and the results from each function.
+
+
+    }
+}
