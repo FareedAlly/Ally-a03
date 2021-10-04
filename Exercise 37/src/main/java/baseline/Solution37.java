81f6ac650d9bca7ac76ac9c09c2af7bea356f454
Fareed <fareedally1999@gmail.com>
2021-10-03 21:56:38 -0400

    completed exercise 37

--- Exercise 37/src/main/java/baseline/Solution37.java
+++ Exercise 37/src/main/java/baseline/Solution37.java
@@ -1,33 +1,79 @@
+/*
+ *  UCF COP3330 Fall 2021 Assignment 3 Solutions
+ *  Copyright 2021 Fareed Ally
+ */
+
 package baseline;
 
 import java.util.Scanner;
+import java.util.concurrent.ThreadLocalRandom;
 
 public class Solution37
 {
     private static final Scanner in = new Scanner(System.in);
 
-    private String passwordGenerator(int length, int special, int number)
+    public String passwordGenerator(int length, int special, int number)
     {
-        // Create strings that stores all the letters, special characters,and numbers
-        // Create variable that is >= numbers + special
-        // Use stringbuilder to generate password
-        // Use for loops to choose characters that will be in password
-        // Return password
+        String alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        String specialCharacters = "!@#$%^&*()_+{}[]<>?,.~";
+        String digits = "1234567890";
+
+        int lettersRequired = number + special;
+
+        length = length + lettersRequired + special + number;
+
+        int trueLength = ThreadLocalRandom.current().nextInt(length, length + 10);
+
+        int count = 0;
+
+        StringBuilder password = new StringBuilder();
+
+        while(count < trueLength)
+        {
+            int letterIndex = ThreadLocalRandom.current().nextInt(0, alphabet.length());
+            password.append(alphabet.charAt(letterIndex));
+
+            count++;
+        }
+
+        for(int i=0; i<special; i++)
+        {
+            int specialIndex = ThreadLocalRandom.current().nextInt(0, specialCharacters.length());
+
+            int index = ThreadLocalRandom.current().nextInt(0, trueLength);
+
+            password.insert(index, specialCharacters.charAt(specialIndex));
+        }
+
+        for(int j=0; j<number; j++)
+        {
+            int numberIndex = ThreadLocalRandom.current().nextInt(0, digits.length());
+
+            int index = ThreadLocalRandom.current().nextInt(0, trueLength);
+
+            password.insert(index, digits.charAt(numberIndex));
+        }
+
+        return password.toString();
     }
 
     private int input(String prompt)
     {
-        // Print prompt
-        // Return value entered
+        System.out.println(prompt);
+
+       return in.nextInt();
     }
 
     public static void main(String[] args)
     {
         Solution37 main = new Solution37();
 
-        // Get all values by calling method
-        // Generate password using method
-        // Print password
+        int length = main.input("What's the minimum length? ");
+        int special = main.input("How many special characters? ");
+        int numbers = main.input("How many numbers? ");
 
+        String password = main.passwordGenerator(length, special, numbers);
+
+        System.out.println("Your password is " + password);
     }
 }
