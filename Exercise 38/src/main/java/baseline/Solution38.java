fe5bc286e45bb9d71fb0c47b731960071f345a5d
Fareed <fareedally1999@gmail.com>
2021-10-03 22:18:03 -0400

    added exercise 38 pseudocode

---
+++ Exercise 38/src/main/java/baseline/Solution38.java
@@ -0,0 +1,38 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution38
+{
+    private static final Scanner in = new Scanner(System.in);
+
+    private int[] filterEvenNumbers(int[] array)
+    {
+        // Use for loop to go through array
+        // Use if statement to keep values that are even. done by dividing by %2.
+        // Store these values in new array and return it
+    }
+
+    private int[] convertToArray(String input)
+    {
+        // Use for loop and parse values to int for each index
+        // return array
+    }
+
+    private String getList(String prompt)
+    {
+        // Print statement asking for list
+        // Return List
+    }
+
+    public static void main(String[] args)
+    {
+        Solution38 main = new Solution38();
+
+        // Get list by calling function
+        // Call function to convert string to array
+        // Call function to return even values in said array
+        // Print results
+
+    }
+}
