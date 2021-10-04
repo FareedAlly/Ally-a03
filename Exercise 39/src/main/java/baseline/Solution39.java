875c381129a7a7c3afbbd7b1f7434ab7ab9cb5c8
Fareed <fareedally1999@gmail.com>
2021-10-03 22:55:44 -0400

    added exercise 39 pseudocode

---
+++ Exercise 39/src/main/java/baseline/Solution39.java
@@ -0,0 +1,49 @@
+package baseline;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Scanner;
+
+public class Solution39
+{
+    private static final Scanner in = new Scanner(System.in);
+
+    private HashMap firstNameMap(String[] lastNames)
+    {
+        // Create hashmap for the firstnames
+        // Put them in order based on lastname
+        // Return hashmap
+    }
+
+    private HashMap positionMap(String[] lastNames)
+    {
+        // Create hashmap for the position
+        // Put them in order based on lastname
+        // Return hashmap
+    }
+
+    private HashMap seperationDateMap(String[] lastNames)
+    {
+        // Create hashmap for the seperation date
+        // Put them in order based on lastname
+        // Return hashmap
+    }
+
+    private void printTable(ArrayList<HashMap<String, String>> employees, String[] lastNames)
+    {
+        // Put hashmaps in order based on table
+        // Print format for table
+        // Use for loop to print each value stored in each hashmap at a specific index
+    }
+
+    public static void main(String[] args)
+    {
+        Solution39 main = new Solution39();
+
+        // Create string that has the last names
+        // Create hashmap and use function to add data for each section
+        // Sort by alphabetic order
+        // Call function to print out table
+
+    }
+}
