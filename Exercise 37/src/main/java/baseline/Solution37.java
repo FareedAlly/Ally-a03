03b65f5814a135cd330e35138ec52cd2b2cc5489
Fareed <fareedally1999@gmail.com>
2021-10-03 21:26:25 -0400

    added exercise 37 pseudocode

---
+++ Exercise 37/src/main/java/baseline/Solution37.java
@@ -0,0 +1,33 @@
+package baseline;
+
+import java.util.Scanner;
+
+public class Solution37
+{
+    private static final Scanner in = new Scanner(System.in);
+
+    private String passwordGenerator(int length, int special, int number)
+    {
+        // Create strings that stores all the letters, special characters,and numbers
+        // Create variable that is >= numbers + special
+        // Use stringbuilder to generate password
+        // Use for loops to choose characters that will be in password
+        // Return password
+    }
+
+    private int input(String prompt)
+    {
+        // Print prompt
+        // Return value entered
+    }
+
+    public static void main(String[] args)
+    {
+        Solution37 main = new Solution37();
+
+        // Get all values by calling method
+        // Generate password using method
+        // Print password
+
+    }
+}
